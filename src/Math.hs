a76991853d038d0b5caa5f03c2468a18670f7e07
Hawk Weisman <hi@hawkweisman.me>
2015-08-20 13:15:13 -0400

    Fix k-nearest

--- src/Math.hs
+++ src/Math.hs
@@ -1,20 +1,19 @@
 {-# LANGUAGE RankNTypes #-}
 
-module Common.Math (
-    kNearest
-    ,
-) where
+module Common.Math ( kNearest ) where
 
 import Data.List
 
 type Dist a = a -> a -> Int
 
-kNearest :: Eq a => Int -> Dist a -> [a] -> [a]
-kNearest k dist xs = findKNearest k' dist (delete nearest xs) [nearest]
-    where nearest = minimumBy dist xs
-          k'         = k - 1
+kNearest :: Eq a => Int -> Dist a -> a -> [a] -> [a]
+kNearest k dist x xs  = findKNearest k' minDist (delete nearest xs) [nearest]
+    where nearest     = minimumBy minDist xs
+          k'          = k - 1
+          minDist a b = compare (distTo a) (distTo b)
+          distTo      = dist x
 
-findKNearest :: Eq a =>  Int -> Dist a -> [a] -> [a] -> [a]
+findKNearest :: Eq a =>  Int -> (a -> a -> Ordering) -> [a] -> [a] -> [a]
 findKNearest k dist xs neighbors = case k of
     0 -> nearest : neighbors
     _ -> findKNearest k' dist xs' (nearest : neighbors)
