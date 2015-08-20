07fbef5ea7ce694f9557c9b514333239bcc9ba7a
Hawk Weisman <hi@hawkweisman.me>
2015-08-19 22:19:38 -0400

    K-nearest almost works

--- src/Math.hs
+++ src/Math.hs
@@ -1,5 +1,23 @@
-module Common.Math where
+{-# LANGUAGE RankNTypes #-}
 
-type Dist a = (Num b) => a -> a -> b
+module Common.Math (
+    kNearest
+    ,
+) where
 
-kNearest :: (Num a) => a -> Dist b -> [b] -> [b]
+import Data.List
+
+type Dist a = a -> a -> Int
+
+kNearest :: Eq a => Int -> Dist a -> [a] -> [a]
+kNearest k dist xs = findKNearest k' dist (delete nearest xs) [nearest]
+    where nearest = minimumBy dist xs
+          k'         = k - 1
+
+findKNearest :: Eq a =>  Int -> Dist a -> [a] -> [a] -> [a]
+findKNearest k dist xs neighbors = case k of
+    0 -> nearest : neighbors
+    _ -> findKNearest k' dist xs' (nearest : neighbors)
+    where nearest    = minimumBy dist xs
+          xs'        = delete nearest xs
+          k'         = k - 1
